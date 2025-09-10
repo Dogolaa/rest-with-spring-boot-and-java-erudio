136866a09e247c2a6a627ec40636eba40af7d8d4
Lucas Dogo <lucasdogo01@gmail.com>
2025-09-10 16:52:52 -0300

    JasperReports, Relatorios Profissionais PART 1

---
+++ 23_JasperReports/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/file/exporter/impl/PdfExporter.java
@@ -0,0 +1,89 @@
+package br.com.erudio.file.exporter.impl;
+
+import br.com.erudio.data.dto.PersonDTO;
+import br.com.erudio.file.exporter.contract.FileExporter;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Component;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Component
+public class PdfExporter implements FileExporter {
+
+    @Override
+    public Resource exportFile(List<PersonDTO> people) throws Exception {
+        InputStream inputStream = getClass().getResourceAsStream("/templates/people.jrxml");
+        if (inputStream == null) {
+            throw new RuntimeException("Template file not found: /templates/people.jrxml");
+        }
+
+        JasperReport jasperReport = JasperCompileManager.compileReport(inputStream);
+
+        JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(people);
+        Map<String, Object> parameters = new HashMap<>();
+        //parameters.put("title", "People Report");
+        JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, dataSource);
+
+        try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
+            JasperExportManager.exportReportToPdfStream(jasperPrint, outputStream);
+            return new ByteArrayResource(outputStream.toByteArray());
+        }
+    }
+
+    @Override
+    public Resource exportPerson(PersonDTO person) throws Exception {
+        InputStream mainTemplateStream = getClass().getResourceAsStream("/templates/person.jrxml");
+        if (mainTemplateStream == null) {
+            throw new RuntimeException("Template file not found: /templates/person.jrxml");
+        }
+        InputStream subReportStream = getClass().getResourceAsStream("/templates/books.jrxml");
+        if (subReportStream == null) {
+            throw new RuntimeException("Template file not found: /templates/books.jrxml");
+        }
+
+        JasperReport mainReport = JasperCompileManager.compileReport(mainTemplateStream);
+        JasperReport subReport = JasperCompileManager.compileReport(subReportStream);
+
+        InputStream qrCodeStream = generateQRCode(person.getProfileUrl(), 200, 200);
+
+
+        JRBeanCollectionDataSource subDataSource = new JRBeanCollectionDataSource(Collections.singletonList(person.getBooks()));
+
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("SUB_REPORT_DATA_SOURCE", subDataSource);
+        parameters.put("BOOK_SUB_REPORT", subReport);
+        parameters.put("QR_CODEIMAGE", qrCodeStream);
+
+        JRBeanCollectionDataSource mainDataSource = new JRBeanCollectionDataSource(Collections.singletonList(person));
+
+        JasperPrint jasperPrint = JasperFillManager.fillReport(mainReport, parameters, mainDataSource);
+
+        try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
+            JasperExportManager.exportReportToPdfStream(jasperPrint, outputStream);
+            return new ByteArrayResource(outputStream.toByteArray());
+        }
+    }
+
+    private InputStream generateQRCode(String url, int widht, int height) throws Exception {
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        BitMatrix bitMatrix = qrCodeWriter.encode(url, BarcodeFormat.QR_CODE, widht, height);
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        MatrixToImageWriter.writeToStream(bitMatrix, "PNG", outputStream);
+
+        return new ByteArrayInputStream(outputStream.toByteArray());
+    }
+}
